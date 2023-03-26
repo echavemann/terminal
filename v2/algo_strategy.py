eda94eeebef4236e3e011b4ef9338547126ff41c
jiale0402 <55820495+jiale0402@users.noreply.github.com>
2023-03-25 19:45:13 -0500

    Update algo_strategy.py

---
+++ v2/algo_strategy.py
@@ -0,0 +1,99 @@
+import gamelib
+import random
+import math
+import warnings
+from sys import maxsize
+import json
+import heapq as pq
+
+"""
+Most of the algo code you write will be in this file unless you create new
+modules yourself. Start by modifying the 'on_turn' function.
+
+Advanced strategy tips: 
+
+  - You can analyze action frames by modifying on_action_frame function
+
+  - The GameState.map object can be manually manipulated to create hypothetical 
+  board states. Though, we recommended making a copy of the map to preserve 
+  the actual current map state.
+"""
+
+class AlgoStrategy(gamelib.AlgoCore):
+    def __init__(self):
+        super().__init__()
+        seed = random.randrange(maxsize)
+        random.seed(seed)
+        gamelib.debug_write('Random seed: {}'.format(seed))
+
+    def on_game_start(self, config):
+        """ 
+        Read in config and perform any initial setup here 
+        """
+        gamelib.debug_write('Configuring your custom algo strategy...')
+        self.config = config
+        global WALL, SUPPORT, TURRET, SCOUT, DEMOLISHER, INTERCEPTOR, MP, SP
+        WALL = config["unitInformation"][0]["shorthand"]
+        SUPPORT = config["unitInformation"][1]["shorthand"]
+        TURRET = config["unitInformation"][2]["shorthand"]
+        SCOUT = config["unitInformation"][3]["shorthand"]
+        DEMOLISHER = config["unitInformation"][4]["shorthand"]
+        INTERCEPTOR = config["unitInformation"][5]["shorthand"]
+        MP = 1
+        SP = 0
+        self.defense_queue = []
+        self.scored_on_locations = []
+                
+    def determine_priority(self):
+        """
+        determine which side we should prioritize according to the locs we have been scored on
+        """
+        self.priority = False
+        if len(self.scored_on_locations) > 0:
+            scored_on_side = max(self.scored_on_count, key=self.scored_on_count.get)
+            count = self.scored_on_count[scored_on_side]
+            self.priority = scored_on_side
+    
+    def on_turn(self, turn_state):
+        """
+        This function is called every turn with the game state wrapper as
+        an argument. The wrapper stores the state of the arena and has methods
+        for querying its state, allocating your current resources as planned
+        unit deployments, and transmitting your intended deployments to the
+        game engine.
+        """
+        game_state = gamelib.GameState(self.config, turn_state)
+        gamelib.debug_write('Performing turn {} of your custom algo strategy'.format(game_state.turn_number))
+        game_state.suppress_warnings(True)  #Comment or remove this line to enable warnings.
+        
+        self.determine_priority()
+        game_state.submit_turn()
+
+    def on_action_frame(self, turn_string):
+        """
+        This is the action frame of the game. This function could be called 
+        hundreds of times per turn and could slow the algo down so avoid putting slow code here.
+        Processing the action frames is complicated so we only suggest it if you have time and experience.
+        Full doc on format of a game frame at in json-docs.html in the root of the Starterkit.
+        """
+        # Let's record at what position we get scored on
+        state = json.loads(turn_string)
+        events = state["events"]
+        breaches = events["breach"]
+        for breach in breaches:
+            location = breach[0]
+            unit_owner_self = True if breach[4] == 1 else False
+            # When parsing the frame data directly, 
+            # 1 is integer for yourself, 2 is opponent (StarterKit code uses 0, 1 as player_index instead)
+            if not unit_owner_self:
+                self.scored_on_locations.append(location)
+                if location[0] <= 5:
+                    self.scored_on_count['left'] += 1
+                elif location[0] >= 22:
+                    self.scored_on_count['right'] += 1
+                else:
+                    self.scored_on_count['mid'] += 1
+
+if __name__ == "__main__":
+    algo = AlgoStrategy()
+    algo.start()
